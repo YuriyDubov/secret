4e2387ad33ca631074028b22878b1d8ad648a63b
Yuriy Dubov <yuri.dubov@raftds.com>
2023-01-27 22:42:07 +0300

    Delete commented code

--- db/client.js
+++ db/client.js
@@ -7,28 +7,6 @@ const pool = new Pool({
     port: 5432,
 });
 
-// const client = new Client({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'secret',
-//     password: '1234',
-//     port: 5432,
-// });
-
-// async function sendQuery(query) {
-//     try {
-//         await client.connect();
-//         const resp = await client.query(query);
-//         await client.end();
-
-//         return resp;
-//     } catch (err) {
-//         console.log(JSON.stringify(err.message));
-//         console.log(`Some error happened during running SQL query: ${query}`);
-//         await client.end();
-//     }
-// }
-
 async function sendQuery(query) {
     const connection = await pool.connect();
 
